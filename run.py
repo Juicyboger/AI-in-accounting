f27466a8ee25b56c5a96cad1867fe093e418d627
Juicyboger <165176916+Juicyboger@users.noreply.github.com>
2025-02-09 17:24:39 +0000

    1

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import os
 from app import app, db
 
+# 获取数据库 URL
+db_url = os.environ.get("DATABASE_URL")
+# 如果使用了旧的 "postgres://" 前缀，则替换为 "postgresql://"
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    # 从环境变量中获取端口号
     port = int(os.environ.get("PORT", 5432))
     app.run(host='0.0.0.0', port=port)
-
