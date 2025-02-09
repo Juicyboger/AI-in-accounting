ee57621d1742beca4874e213d9f2c1d647462711
Juicyboger <165176916+Juicyboger@users.noreply.github.com>
2025-02-09 18:15:58 +0000

    1

--- run.py
+++ run.py
@@ -1,15 +1,9 @@
+import os
 from app import app, db
 
-with app.app_context():
-    db.create_all()
-
-from app import db
-from app.models import User
-
-# 创建管理员账号，用户名为 admin，密码为 admin123（请根据需要更改）
-admin = User(username='admin', password='admin123', is_admin=True)
-db.session.add(admin)
-db.session.commit()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5432)
+    # 在主程序中创建应用上下文，并初始化数据库
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
