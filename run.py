e59ca9303ce11512759880edf01f37195e3d6633
Juicyboger <165176916+Juicyboger@users.noreply.github.com>
2025-02-09 18:19:17 +0000

    1

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
-import os
 from app import app, db
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    # 在主程序中创建应用上下文，并初始化数据库
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
